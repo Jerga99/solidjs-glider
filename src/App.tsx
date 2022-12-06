4dfd0ca7c25c129f39c43b787f36b11be51aeaf6
Filip Jerga <jerga99@gmail.com>
2022-12-07 12:38:48 +1300

    Glide post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Component, createSignal, createUniqueId } from "solid-js";
-import { AiOutlineMessage } from "solid-icons/ai";
-import { FiTrash } from "solid-icons/fi";
-import { FaRegularImage, FaRegularHeart } from "solid-icons/fa";
+import { Component, createSignal, createUniqueId, For } from "solid-js";
+import { FaRegularImage } from "solid-icons/fa";
 import MainLayout from "./components/layouts/Main";
+import GlidePost from "./components/glides/GlidePost";
 
 type Glide = {
   content: string;
@@ -83,47 +82,11 @@ const App: Component = () => {
         {/* MESSENGER END */}
       </div>
       <div class="h-px bg-gray-700 my-1" />
-      {/* GLIDE POST START */}
-      <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
-        <div class="flex-it flex-row">
-          <div class="flex-it mr-4">
-            <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-              <img
-                class="rounded-full"
-                src="https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png"
-              ></img>
-            </div>
-          </div>
-          <article class="flex-it flex-grow flex-shrink cursor-pointer">
-            <div class="flex-it justify-center flex-grow mb-1">
-              <div class="flex-it justify-between flex-row w-full">
-                <div>
-                  <span class="font-bold">Filip99</span>
-                  <span class="mx-2">&#8226;</span>
-                  <span class="text-gray-400">2h</span>
-                </div>
-                <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
-                  <FiTrash size={16} />
-                </div>
-              </div>
-            </div>
-            <div class="flex-it flex-row flex-grow-0 items-center mb-2">
-              <div class="flex-it mr-3 mb-3 w-full">My First Post</div>
-            </div>
-            <div class="flex-it flex-row flex-grow text-gray-400">
-              <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-blue-400">
-                <AiOutlineMessage size={18} />
-                <span class="text-xs ml-3">321</span>
-              </div>
-              <div class="flex-it flex-row items-center cursor-pointer transition hover:text-pink-400">
-                <FaRegularHeart size={18} />
-                <span class="text-xs ml-3">123</span>
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
-      {/* GLIDE POST END */}
+      <For each={glides()}>
+        { (glide) =>
+          <GlidePost />
+        }
+      </For>
       {/* HOME PAGE END */}
     </MainLayout>
   );
---
+++ src/components/glides/GlidePost.tsx
@@ -0,0 +1,51 @@
+import { AiOutlineMessage } from "solid-icons/ai";
+import { FaRegularHeart } from "solid-icons/fa";
+import { FiTrash } from "solid-icons/fi";
+
+const GlidePost = () => {
+
+
+  return (
+    <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
+      <div class="flex-it flex-row">
+        <div class="flex-it mr-4">
+          <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+            <img
+              class="rounded-full"
+              src="https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png"
+            ></img>
+          </div>
+        </div>
+        <article class="flex-it flex-grow flex-shrink cursor-pointer">
+          <div class="flex-it justify-center flex-grow mb-1">
+            <div class="flex-it justify-between flex-row w-full">
+              <div>
+                <span class="font-bold">Filip99</span>
+                <span class="mx-2">&#8226;</span>
+                <span class="text-gray-400">2h</span>
+              </div>
+              <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
+                <FiTrash size={16} />
+              </div>
+            </div>
+          </div>
+          <div class="flex-it flex-row flex-grow-0 items-center mb-2">
+            <div class="flex-it mr-3 mb-3 w-full">My First Post</div>
+          </div>
+          <div class="flex-it flex-row flex-grow text-gray-400">
+            <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-blue-400">
+              <AiOutlineMessage size={18} />
+              <span class="text-xs ml-3">321</span>
+            </div>
+            <div class="flex-it flex-row items-center cursor-pointer transition hover:text-pink-400">
+              <FaRegularHeart size={18} />
+              <span class="text-xs ml-3">123</span>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  )
+}
+
+export default GlidePost;
