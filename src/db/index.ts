f3c308267471ab5d444df9f8cf7ecee6d0d2a23c
Filip Jerga <jerga99@gmail.com>
2022-12-19 10:53:24 +1300

    Init Firebase

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClRttOUJdLjxImhSldJMjwaya_llM8SFM",
+  authDomain: "glider-197b2.firebaseapp.com",
+  projectId: "glider-197b2",
+  storageBucket: "glider-197b2.appspot.com",
+  messagingSenderId: "779541992064",
+  appId: "1:779541992064:web:a9971e2cb53483b9b10c65"
+};
+
+const app = initializeApp(firebaseConfig);
