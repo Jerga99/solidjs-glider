298670292597239cbe5b308fc4b88efb54db2ec2
Filip Jerga <jerga99@gmail.com>
2022-12-20 09:05:32 +1300

    Extend User Type

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,13 @@
+import { DocumentReference } from "firebase/firestore";
 
 
 export interface User {
+  uid: string;
   nickName: string;
+  fullName: string;
   avatar: string;
+  followers: DocumentReference[];
+  following: DocumentReference[];
+  followersCount: number;
+  followingCount: number;
 }
