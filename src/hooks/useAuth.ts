6b818fa818869ee2fc833ee2ef974614ec03742b
Filip Jerga <jerga99@gmail.com>
2022-12-26 09:16:02 +1300

    Display snack on authentication

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,18 +1,21 @@
 import { FirebaseError } from "firebase/app";
 import { createSignal } from "solid-js";
 import { authenticate, AuthType } from "../api/auth";
+import { useUIDispatch } from "../context/ui";
 import { AuthForm } from "../types/Form";
 
 const useAuth = (authType: AuthType) => {
   const [loading, setLoading] = createSignal(false);
+  const {addSnackbar} = useUIDispatch();
 
   const authUser = async (form: AuthForm) => {
     setLoading(true);
     try {
       await authenticate(form, authType);
+      addSnackbar({message: "Welcome to Glider", type: "success"});
     } catch(error) {
       const message = (error as FirebaseError).message;
-      console.log(message);
+      addSnackbar({message, type: "error"});
     } finally {
       setLoading(false);
     }
