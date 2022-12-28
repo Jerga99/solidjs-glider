12fd7f9b350277b14d6cd80be6bf4223cb351578
Filip Jerga <jerga99@gmail.com>
2022-12-29 11:00:35 +1300

    Stop Loading Condition

--- src/hooks/useGlides.ts
+++ src/hooks/useGlides.ts
@@ -25,6 +25,10 @@ const useGlides = () => {
 
   const loadGlides = async () => {
     const _page = page();
+
+    if (_page > 1 && !store.lastGlide) {
+      return;
+    }
     
     setStore("loading", true);
     try {
