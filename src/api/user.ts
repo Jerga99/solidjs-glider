931677e3afc38613f54fbe544417760c73b48db0
Filip Jerga <jerga99@gmail.com>
2022-12-30 19:47:55 +1300

    Get users

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,18 @@
 
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../db";
+import { User } from "../types/User";
+
 const getUsers = async () => {
-  console.log("fetching users!");
-  return [];
+  const q = query(collection(db, "users"));
+  const querySnapshot = await getDocs(q);
+
+  const users = querySnapshot.docs.map((doc) => {
+    const user = doc.data() as User;
+    return user;
+  })
+
+  return users;
 }
 
 export {
