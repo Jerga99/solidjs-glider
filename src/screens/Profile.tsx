ac6d7109e244b1cd1aca8b70de174a589d4dfb00
Filip Jerga <jerga99@gmail.com>
2022-12-31 13:52:07 +1300

    Display message when following all users

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -36,42 +36,53 @@ const ProfileScreen: Component = () => {
           when={!loading()}
           fallback={<CenteredDataLoader />}
         >
-          <For each={users()}>
-            {(user) => (
-              <div class="flex-it p-4">
-                <div class="flex-it flex-row">
-                  <div class="flex-it mr-4">
-                    <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-                      <img class="rounded-full" src={user.avatar}></img>
-                    </div>
-                  </div>
-                  <article class="flex-it flex-grow flex-shrink">
-                    <div class="flex-it justify-center flex-grow mb-1">
-                      <div class="flex-it justify-between flex-row w-full">
-                        <div class="flex-it justify-center">
-                          <span class="font-bold">{user.nickName}</span>
-                        </div>
-                        <div class="flex-it w-32 mt-3 cursor-pointer">
-                          <button
-                            disabled={loadingFollow()}
-                            onClick={() => followUser(user)}
-                            type="button"
-                            class="
-                                disabled:cursor-not-allowed disabled:bg-gray-400
-                                bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
-                          >
-                            <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-                              <span>Follow</span>
-                            </div>
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </article>
+          <Show 
+            when={users().length > 0}
+            fallback={
+              <div class="flex-it">
+                <div class="bg-yellow-500 mt-6 p-2 rounded-lg mx-4">
+                  You follow all users in the world...
                 </div>
               </div>
-            )}
-          </For>
+            }
+          >
+            <For each={users()}>
+              {(user) => (
+                <div class="flex-it p-4">
+                  <div class="flex-it flex-row">
+                    <div class="flex-it mr-4">
+                      <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+                        <img class="rounded-full" src={user.avatar}></img>
+                      </div>
+                    </div>
+                    <article class="flex-it flex-grow flex-shrink">
+                      <div class="flex-it justify-center flex-grow mb-1">
+                        <div class="flex-it justify-between flex-row w-full">
+                          <div class="flex-it justify-center">
+                            <span class="font-bold">{user.nickName}</span>
+                          </div>
+                          <div class="flex-it w-32 mt-3 cursor-pointer">
+                            <button
+                              disabled={loadingFollow()}
+                              onClick={() => followUser(user)}
+                              type="button"
+                              class="
+                                  disabled:cursor-not-allowed disabled:bg-gray-400
+                                  bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
+                            >
+                              <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
+                                <span>Follow</span>
+                              </div>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </article>
+                  </div>
+                </div>
+              )}
+            </For>
+          </Show>
         </Show>
       </div>
     </MainLayout>
