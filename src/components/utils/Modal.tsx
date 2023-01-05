3f12fe1697c2c160a530a023b5cb3c8661a58a28
Filip Jerga <jerga99@gmail.com>
2023-01-06 08:23:17 +1300

    Close Modal on outside click

--- src/components/utils/Modal.tsx
+++ src/components/utils/Modal.tsx
@@ -15,13 +15,21 @@ type Props = {
 const Modal: ParentComponent<Props> = (props) => {
   const [isOpen, setOpen] = createSignal(false);
 
+  let modalRef: HTMLDivElement;
+
   return (
     <>
       <props.openComponent setOpen={setOpen} />
       <Show when={isOpen()}>
         <Portal>
-          <div class="openModal">
-            <div class="modal fixed min-w-160 top-14 left-2/4 p-8 -translate-x-1/2 rounded-2xl">
+          <div 
+            onClick={(e) => {
+              if (!modalRef.contains(e.target)) {
+                setOpen(false);
+              }
+            }}
+            class="openModal">
+            <div ref={modalRef!} class="modal fixed min-w-160 top-14 left-2/4 p-8 -translate-x-1/2 rounded-2xl">
               {props.children}
             </div>
           </div>
