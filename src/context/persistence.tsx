9cfa963f95efed9063c4b24e54e9f949ae119e6b
Filip Jerga <jerga99@gmail.com>
2023-01-09 19:14:35 +1300

    Adds functionality to clear persistence store

--- src/context/persistence.tsx
+++ src/context/persistence.tsx
@@ -18,10 +18,16 @@ type PersistenceContextType = {
 
 const PersistenceContext = createContext<PersistenceContextType>();
 
+const PERSISTENCE_LIMIT = 50;
+
 const PersistenceProvider: ParentComponent = (props) => {
   const [store, setStore] = createStore<PersistenceStore>();
 
   const setValue = (key: string, value: any) => {
+    if (Object.keys(store).length > PERSISTENCE_LIMIT) {
+      clear();
+    }
+
     setStore(produce(store => {
       store[key] = value;
     }));
@@ -29,6 +35,14 @@ const PersistenceProvider: ParentComponent = (props) => {
 
   const getValue = <T, >(key: string) => {
     return store[key] as T;
+  }
+
+  const clear = () => {
+    setStore(produce(store => {
+      Object.keys(store).forEach(key => {
+        delete store[key];
+      })
+    }))
   }
 
   const hasValue = (key: string) => !!store[key];
@@ -71,6 +85,7 @@ const PersistenceProvider: ParentComponent = (props) => {
     }
 
     const result = await getData();
+    setValue(key, result);
     return result;
   }
   
